786d4e274012bbeeaea12381f99a8f23147ce603
adgor <adgor000@gmail.com>
2020-01-05 11:57:30 +0100

    adding topbar

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+// Promotion msg
+const promo = document.querySelector("#promotion");
+// Ndro Promo msg
+promo.childNodes[1].textContent =
+  "Free domain name for 1 year with our annual hosting plans.  *JS change me* ";
