e51992758e1b3f9585413c7a10a43bfb8ab0de71
adgor <adgor000@gmail.com>
2020-06-26 22:21:59 +0200

    H1

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,3 +3,13 @@ const promo = document.querySelector("#promotion");
 // Ndro Promo msg
 promo.childNodes[1].textContent =
   "Free domain name for 1 year with our annual hosting plans.  *JS change me* ";
+
+
+
+const menuTarget = document.querySelector('[data-menu-toggle]');
+const menu = document.querySelector('[data-menu]');
+
+menuTarget.addEventListener('click', event => {
+  event.preventDefault()
+  menu.classList.toggle('hidden')
+})
